b595e2a6a41ee862322d90c04ae4852b27224ca9
David M. Anderson <dma@epsilondelta.us>
2019-12-25 10:24:05 -0800

    Added button to open calculator in a new window

--- client/calculator/calcPage.js
+++ client/calculator/calcPage.js
@@ -21,6 +21,35 @@ import runningCalculator from './runningCalculator.js';
 function run( ) {
     $( 'h1' ).html( "Runner's Calculator" ); //eslint-disable-line 
     runningCalculator.run( $('main') );
+    addOpenWindowButton( );
+}
+
+//=============================================================================
+
+let calculatorWindow = null;
+
+function addOpenWindowButton( ) {
+    const $div = $( '<div>' )
+        .css( 'text-align', 'center' );
+    const $btn = $( '<button>' )
+        .attr( 'type', 'button' )
+        .text( 'Open in a New Window' );
+    $div.append( $btn );
+    $( 'main' ).append( $div );
+    $btn.on( 'click', openWindow );
+
+    //-------------------------------------------------------------------------
+
+    function openWindow( ) {
+        if ( !calculatorWindow || calculatorWindow.closed ) {
+            const url = '/calculator';
+            const windowName = 'runnersCalculatorWindow';
+            const options = 'width=380,height=580,resizable,scrollbars';
+            calculatorWindow = window.open( url, windowName, options );
+        } else {
+            calculatorWindow.focus( );
+        }
+    }
 }
 
 //=============================================================================
